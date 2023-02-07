86e4d6055691de03590ba0b8753111a727b6ee60
Yuansheng Zhu <yuz128@pitt.edu>
2023-02-07 11:06:30 -0500

    Delete scripts directory

--- scripts/run_main_unseen_2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd ../src
-CUDA_VISIBLE_DEVICES=4 python main_OpenVAD_EDL.py --unseen-anomaly 4 5 --id rgb-2-1 --modality RGB --feature-size 1024
-CUDA_VISIBLE_DEVICES=4 python main_OpenVAD_EDL.py --unseen-anomaly 0 1 --id rgb-2-2 --modality RGB --feature-size 1024
-CUDA_VISIBLE_DEVICES=4 python main_OpenVAD_EDL.py --unseen-anomaly 4 6 --id rgb-2-3 --modality RGB --feature-size 1024
-popd
-
--- scripts/run_main_unseen_3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pushd ../src
-CUDA_VISIBLE_DEVICES=5 python main_OpenVAD_EDL.py --unseen-anomaly 0 1 3 --id rgb-3-1 --modality RGB --feature-size 1024
-CUDA_VISIBLE_DEVICES=5 python main_OpenVAD_EDL.py --unseen-anomaly 3 4 5 --id rgb-3-2 --modality RGB --feature-size 1024
-CUDA_VISIBLE_DEVICES=5 python main_OpenVAD_EDL.py --unseen-anomaly 0 1 3 --id rgb-3-3 --modality RGB --feature-size 1024
-popd
-
